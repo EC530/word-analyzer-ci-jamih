d9a2aa73abe7051945dea9e9db7b4472f0536adc
Jami <jamih@bu.edu>
2022-01-27 23:42:20 -0500

    wrote pytest for counting words

--- word_analyzer.py
+++ word_analyzer.py
@@ -2,43 +2,55 @@ from matplotlib import pyplot as plt
 import numpy as np
 import re
 
+def count_words(n):
+    f = open(n, "r")
+    #print(f.read())
 
-f = open("poem.txt", "r")
-#print(f.read())
-
-# Create an empty dictionary
-d = dict()
-  
-# Loop through each line of the file
-for line in f:
+    # Create an empty dictionary
+    d = dict()
     
-  
-    # Convert the characters in line to uppercase
-    # to avoid case mismatch
-    line = line.upper()
-    '''
-    # Remove the leading and trailing spaces
-    line = line.strip()
-  
-    # Split the line into a list of words
-    words = line.split(" ")
-    '''
-    # using res to filter string and extract punctuation and symbols
-    res = re.findall(r'\w+', line)
+    # Loop through each line of the file
+    for line in f:
+        
+    
+        # Convert the characters in line to uppercase
+        # to avoid case mismatch
+        line = line.upper()
+        '''
+        # Remove the leading and trailing spaces
+        line = line.strip()
+    
+        # Split the line into a list of words
+        words = line.split(" ")
+        '''
+        # using res to filter string and extract punctuation and symbols
+        res = re.findall(r'\w+', line)
 
-    # Iterate over each word in line
-    for word in res:
-        # Check if the word is already in dictionary
-        if word in d:
-            # Increment count of word by 1
-            d[word] = d[word] + 1
-        else:
-            # Add the word to dictionary with count 1
-            d[word] = 1
-  
-# Print the contents of dictionary
-for key in list(d.keys()):
-    print(key, ":", d[key])
+        # Iterate over each word in line
+        for word in res:
+            # Check if the word is already in dictionary
+            if word in d:
+                # Increment count of word by 1
+                d[word] = d[word] + 1
+            else:
+                # Add the word to dictionary with count 1
+                d[word] = 1
+    
+    # Print the contents of dictionary
+    for key in list(d.keys()):
+        print(key, ":", d[key])
+
+    return d
+
+def main():
+    count_words(poem.txt)
+    return
+
+
+
+
+
+
 
 '''
 # Creating dataset
