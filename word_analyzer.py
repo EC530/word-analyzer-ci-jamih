665d260da0ffca80991826e351ba5945006c6a92
Jami <jamih@bu.edu>
2022-01-30 16:28:42 -0500

    adding NLTK stop words

--- word_analyzer.py
+++ word_analyzer.py
@@ -10,11 +10,34 @@ def txt_count_words(n):
     # Create an empty dictionary
     d = dict()
 
+    # Array of NLTK English Stopwords
+    stop_words = ["i", "me", "my", "myself", "we",
+                  "our", "ours", "ourselves", "you",
+                  "your", "yours", "yourself", "yourselves",
+                  "he", "him", "his", "himself", "she", "her",
+                  "hers", "herself", "it", "its", "itself", "they",
+                  "them", "their", "theirs", "themselves", "what",
+                  "which", "who", "whom", "this", "that", "these",
+                  "those", "am", "is", "are", "was", "were", "be",
+                  "been", "being", "have", "has", "had", "having",
+                  "do", "does", "did", "doing", "a", "an", "the",
+                  "and", "but", "if", "or", "because", "as", "until",
+                  "while", "of", "at", "by", "for", "with", "about",
+                  "against", "between", "into", "through", "during",
+                  "before", "after", "above", "below", "to", "from",
+                  "up", "down", "in", "out", "on", "off", "over",
+                  "under", "again", "further", "then", "once", "here",
+                  "there", "when", "where", "why", "how", "all", "any",
+                  "both", "each", "few", "more", "most", "other", "some",
+                  "such", "no", "nor", "not", "only", "own", "same", "so",
+                  "than", "too", "very", "s", "t", "can", "will", "just",
+                  "don", "should", "now"]
+
     # Loop through each line of the file
     for line in f:
-        # Convert the characters in line to uppercase
+        # Convert the characters in line to lowercase
         # to avoid case mismatch
-        line = line.upper()
+        line = line.lower()
 
         # using res to filter string and extract punctuation and symbols
         res = re.findall(r'\w+', line)
@@ -22,12 +45,16 @@ def txt_count_words(n):
         # Iterate over each word in line
         for word in res:
             # Check if the word is already in dictionary
-            if word in d:
-                # Increment count of word by 1
-                d[word] = d[word] + 1
-            else:
-                # Add the word to dictionary with count 1
-                d[word] = 1
+            print(word)
+            print(word in stop_words)
+            # Only put word in dictionary if not a stop word
+            if not word in stop_words:
+                if word in d:
+                    # Increment count of word by 1
+                    d[word] = d[word] + 1
+                else:
+                    # Add the word to dictionary with count 1
+                    d[word] = 1
 
     return d
 
@@ -35,6 +62,7 @@ def txt_count_words(n):
 def txt_histogram(d):
     # Create an empty array for words
     words = []
+
 
     # Create an empty array for frequency of words
     freq = []
